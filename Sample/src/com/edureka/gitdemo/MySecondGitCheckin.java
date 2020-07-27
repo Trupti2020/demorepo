ac1ce6ae9e227b814c6b4c284a97f36fc357ccca
Trupti2020 <trupti.korhale@gmail.com>
2020-07-27 18:02:16 +0530

    back to original file

--- Sample/src/com/edureka/gitdemo/MySecondGitCheckin.java
+++ Sample/src/com/edureka/gitdemo/MySecondGitCheckin.java
@@ -5,10 +5,7 @@ public class MySecondGitCheckin {
 	public static void main(String[] args) {
 		
 		System.out.println("Added new class file");
-<<<<<<< HEAD
-		// demo of gitignore 
-=======
->>>>>>> 6fe00a961016febca141a1b9be73d585c93aad6d
+
 
 	}
 
